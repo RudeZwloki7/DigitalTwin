111aa1eab940c5ad1e9a536a0b492e8d5c8f601e
RudeZwloki7 <rudezwloki7@gmail.com>
2021-06-02 16:08:37 +0300

    Lamp demo

---
+++ Assets/Scripts/LampObject.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+//using Quickstarts.ConsoleReferenceClient;
+
+public class LampObject : MonoBehaviour
+{
+    public bool _isLit = false;
+
+    public bool IsLit
+    {
+        get => _isLit;
+        private set => _isLit = value;
+    }
+
+    public GameObject filament;
+
+    private Material _wolfram;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _wolfram = filament.GetComponent<Renderer>().material;
+        if (IsLit)
+        {
+            _wolfram.DisableKeyword("_EMISSION");   
+        }
+        else
+        {
+            _wolfram.EnableKeyword("_EMISSION");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _wolfram.EnableKeyword("_EMISSION");
+            IsLit = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _wolfram.DisableKeyword("_EMISSION");
+            IsLit = false;
+        }
+    }
+}
